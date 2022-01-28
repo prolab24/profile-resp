cc4c0d94f401e2534efbea47966fae772e01ee5a
Aashish Shrestha <97799671+prolab24@users.noreply.github.com>
2022-01-28 22:47:18 +0545

    Add files via upload

---
+++ javascript/click.js
@@ -0,0 +1,25 @@
+var first_like = document.getElementById('star-1');
+var first_color = document.getElementById('first_color');
+first_color.onclick=function(){
+  first_like.classList.toggle('black');
+}
+var second_like = document.getElementById('star-2');
+var second_color = document.getElementById('second_color');
+second_color.onclick=function(){
+  second_like.classList.toggle('black');
+}
+var third_like = document.getElementById('star-3');
+var third_color = document.getElementById('third_color');
+third_color.onclick=function(){
+  third_like.classList.toggle('black');
+}
+var four_like = document.getElementById('star-4');
+var four_color = document.getElementById('four_color');
+four_color.onclick=function(){
+  four_like.classList.toggle('black');
+}
+var fifth_like = document.getElementById('star-5');
+var fifth_color = document.getElementById('fifth_color');
+fifth_color.onclick=function(){
+fifth_like.classList.toggle('black');
+}
